c8e3f14bda124766b99f2632894883795e3e2ac7
Isaac Sanders <isaac@isaacbfsanders.com>
2012-06-18 13:43:06 -0400

    Added Stripe specfic information

--- lib/omniauth/strategies/stripe_platform.rb
+++ lib/omniauth/strategies/stripe_platform.rb
@@ -3,6 +3,11 @@ require 'omniauth/strategies/oauth2'
 module OmniAuth
   module Strategies
     class StripePlatform < OmniAuth::Strategies::OAuth2
+      option :name, 'twitter'
+      option :client_options, {
+        :authorize_path => '/oauth/authorize',
+        :site => 'https://manage.stripe.com'
+      }
     end
   end
 end
