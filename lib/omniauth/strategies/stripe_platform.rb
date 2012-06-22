d62da1bebfc208eb43276296b0722d75b2c61a27
Isaac Sanders <isaac@isaacbfsanders.com>
2012-06-22 15:15:42 -0400

    Added some integration info.

--- lib/omniauth/strategies/stripe_platform.rb
+++ lib/omniauth/strategies/stripe_platform.rb
@@ -12,6 +12,32 @@ module OmniAuth
       option :client_options, {
         :site => 'https://manage.stripe.com'
       }
+
+      uid { raw_info[:stripe_user_id] }
+
+      info do
+        {
+          :scope => raw_info[:scope],
+          :livemode => raw_info[:livemode],
+          :access_token => raw_info[:access_token],
+          :expires_in => raw_info[:expires_in],
+          :stripe_publishable_key => raw_info[:stripe_publishable_key]
+        }
+      end
+
+      extra do
+        {
+          :raw_info => raw_info
+        }
+      end
+
+      def raw_info
+        if access_token.expires? && access_token.expired?
+          @raw_info = access_token.refresh!
+        else
+          @raw_info ||= access_token.post(access_token.client.token_url).parsed || {}
+        end
+      end
     end
   end
 end
