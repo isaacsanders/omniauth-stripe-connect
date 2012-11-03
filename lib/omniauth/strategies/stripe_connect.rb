06571917d122f21b7373fdad2940a0ee1e756225
Chris Oliver <excid3@gmail.com>
2012-11-03 14:10:14 -0500

    Enable passing of stripe_landing and stripe_user fields in the URL

--- lib/omniauth/strategies/stripe_connect.rb
+++ lib/omniauth/strategies/stripe_connect.rb
@@ -40,6 +40,16 @@ module OmniAuth
         verifier = request.params['code']
         client.auth_code.get_token(verifier, {:redirect_uri => callback_url}.merge(token_params.to_hash(:symbolize_keys => true)).merge(headers))
       end
+
+      alias :old_request_phase :request_phase
+      def request_phase
+        options[:authorize_params].merge!(
+          :stripe_landing => session["omniauth.params"]["stripe_landing"],
+          :stripe_user => session["omniauth.params"]["stripe_user"]
+        )
+        p options
+        old_request_phase
+      end
     end
   end
 end
