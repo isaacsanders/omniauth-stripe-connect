f4239166642f2cfa94597ab9f0b06213f64c0112
Szymon Kurcab <szymon.kurcab@gmail.com>
2021-08-23 09:48:03 -0400

    Blacklist '_method' request param

--- lib/omniauth/strategies/stripe_connect.rb
+++ lib/omniauth/strategies/stripe_connect.rb
@@ -64,7 +64,7 @@ module OmniAuth
 
       def authorize_params
         params = super
-        params = params.merge(request.params) unless OmniAuth.config.test_mode
+        params = params.merge(request_params) unless OmniAuth.config.test_mode
         redirect_params.merge(params)
       end
 
@@ -86,6 +86,14 @@ module OmniAuth
       def build_access_token
         verifier = request.params['code']
         client.auth_code.get_token(verifier, token_params)
+      end
+
+      def request_params
+        request.params.except(*request_blacklisted_params)
+      end
+
+      def request_blacklisted_params
+        %w(_method)
       end
     end
   end
